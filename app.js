cd27bf75e7ddd33ee27aa9b6cfa8d415a5876335
kannika150799 <kannika_1507@hotmail.com>
2021-01-07 19:57:22 +0700

    init git

---
+++ app.js
@@ -0,0 +1,45 @@
+var modal = document.getElementById("myModal");
+var img1 = document.getElementById("myImg1");
+var img2 = document.getElementById("myImg2");
+var img3 = document.getElementById("myImg3");
+var img4 = document.getElementById("myImg4");
+var img5 = document.getElementById("myImg5");
+var modalImg = document.getElementById("img01");
+
+img1.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+};
+
+img2.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+};
+
+img3.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+};
+
+img4.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+};
+
+img5.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+};
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+function myFunction() {
+    window.open(
+    "https://siamceladon.com/",
+    "_blank",
+    "toolbar=yes,scrollbars=yes,resizable=yes,width=600,height=600"
+  );
+}
